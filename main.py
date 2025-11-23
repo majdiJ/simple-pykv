9d034442f937fb3076a9814c9b886b7e087eab1f
majdiJ <contact@majdij.com>
2025-11-23 21:48:36 +0000

    Initital python files

---
+++ main.py
@@ -0,0 +1,9 @@
+# Simple PyKV - A Lightweight Key-Value Store Server in Python
+
+def program_start_message():
+    print("Simple PyKV Server is starting...")
+    # Just welcome the user, legal, privacy/security info, etc
+
+if __name__ == "__main__":
+    print("Simple PyKV - Version 0.0.0")
+    program_start_message()
