2f49910bed4b5d822073b6d21bf9470dfadb78ac
majdiJ <contact@majdij.com>
2025-12-09 21:01:30 +0000

    Improve system_info class to follow Python OOP conventions (As part of Issue #2)

--- utils/http_response.py
+++ utils/http_response.py
@@ -5,7 +5,7 @@ def create_response(message, status_code=200, system_info: system_info = None, d
     response = {
         "status_code": status_code,
         "message": message,
-        "System": system_info.get_system_info() if system_info else None,
+        "System": system_info.system_info if system_info else None,
         "data": data
     }
     return jsonify(response), status_code
